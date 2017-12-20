5b2df7816a10c44be909dc73edd76f48d9e4c665
Andrey Makar-Uvarov <andrey.makar-uvarov@hugo.events>
2017-12-21 01:31:36 +0300

    fx

--- src/app/distance/cendaly.ts
+++ src/app/distance/cendaly.ts
@@ -8,11 +8,12 @@ export default function cendaly(x, y) {
     const n = x.length;
     let delta = 0;
 
-    const x1 = x.slice(0).sort();
-    const y1 = y.slice(0).sort();
+    const x1 = x;
+    const y1 = y;
 
     x1.forEach((item, key) => {
         let nextDelta;
+        let nextDelta2;
 
         if (x1[key] === y1[key]) {
             nextDelta = 0;
@@ -26,12 +27,22 @@ export default function cendaly(x, y) {
             nextDelta = -1;
         }
 
-        delta += (nextDelta * 2);
+        if (y1[key] === x1[key]) {
+            nextDelta2 = 0;
+        }
+
+        if (y1[key] > x1[key]) {
+            nextDelta2 = 1;
+        }
+
+        if (y1[key] < x1[key]) {
+            nextDelta2 = -1;
+        }
+
+        delta += (nextDelta * nextDelta2);
     });
 
-    console.log(delta);
-    console.log(1 - (2 / (n * (n - 1))));
-    console.log('-----');
+
 
     return 1 - (2 / (n * (n - 1))) * delta;
 }
